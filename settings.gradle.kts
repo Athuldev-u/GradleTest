a18dab14a57e765d48d7bb6ed8bc9a29e619cfe3
Athuldev Uchummel(UST <310960@ust.com>
2026-07-09 19:41:50 +0530

    order test 16

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,9 @@
+
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+        publishing.onlyIf { true }
+    }
+}
 rootProject.name = "GradleTest"
